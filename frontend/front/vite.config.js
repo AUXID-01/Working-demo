69ce0d3ffa9bfb4520897461fda37c4538815d90
AUXID-01 <ayushpatra240@gmail.com>
2025-09-17 11:40:10 +0530

    dockerized

--- frontend/front/vite.config.js
+++ frontend/front/vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig({
+/*export default defineConfig({
   plugins: [react()],
-})
+})*/
+
+export default {
+  // other config...
+  server: {
+    host: true, // or '0.0.0.0'
+    allowedHosts: ['frontend', 'localhost', 'your-other-hosts'],
+  }
+}
+
